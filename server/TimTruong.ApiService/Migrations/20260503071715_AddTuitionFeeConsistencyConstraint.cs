cc5d764de042edfdbf07b535a489d8a3f79a2ddc
longhuynhdev <longhuynhdev@gmail.com>
2026-05-03 14:21:37 +0700

    feat: add database CHECK constraint for TuitionFeeAmount and TuitionFeeUnit

---
+++ server/TimTruong.ApiService/Migrations/20260503071715_AddTuitionFeeConsistencyConstraint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimTruong.ApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTuitionFeeConsistencyConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""Majors""
+                ADD CONSTRAINT ""CK_Majors_TuitionFee_Consistency""
+                CHECK (
+                    (""TuitionFeeAmount"" IS NULL AND ""TuitionFeeUnit"" IS NULL) OR
+                    (""TuitionFeeAmount"" IS NOT NULL AND ""TuitionFeeUnit"" IS NOT NULL)
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""Majors""
+                DROP CONSTRAINT ""CK_Majors_TuitionFee_Consistency""
+            ");
+        }
+    }
+}
