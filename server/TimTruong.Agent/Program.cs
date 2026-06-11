63d5564b636f86e3da451c67871fdfdcf7647cf9
longhuynhdev <longhuynhdev@gmail.com>
2026-06-11 09:05:20 +0700

    feat(agent): add initial implementation of ChatClientAgent with configuration setup

---
+++ server/TimTruong.Agent/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddUserSecrets<Program>();
+    
+IConfiguration config = builder.Build();
+
+var apiKey = config.GetValue<string>("ApiKey");
+var model = "gemini-3.1-flash-lite";
+
+IChatClient client = new Google.GenAI.Client(apiKey: apiKey).AsIChatClient(model);
+ChatClientAgent agent = new ChatClientAgent(client);
+
+AgentResponse response = await agent.RunAsync("What is the capital of VietNam?");
+
+Console.WriteLine(response);
