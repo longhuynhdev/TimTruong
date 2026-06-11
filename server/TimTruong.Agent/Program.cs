d806bc2df8ae927dc2d314219c528cb705d62e71
longhuynhdev <longhuynhdev@gmail.com>
2026-06-11 09:24:01 +0700

    agent: add session

--- server/TimTruong.Agent/Program.cs
+++ server/TimTruong.Agent/Program.cs
@@ -12,8 +12,24 @@ var apiKey = config.GetValue<string>("ApiKey");
 var model = "gemini-3.1-flash-lite";
 
 IChatClient client = new Google.GenAI.Client(apiKey: apiKey).AsIChatClient(model);
-ChatClientAgent agent = new ChatClientAgent(client);
 
-AgentResponse response = await agent.RunAsync("What is the capital of VietNam?");
+ChatClientAgent agent =
+    client.AsAIAgent(
+        instructions:"You are a friendly assistant. Keep your answers brief. And your answer needs to be in Vietnamese",
+        name: "HelloAgent",
+        tools: []
+        );
 
-Console.WriteLine(response);
+AgentSession session = await agent.CreateSessionAsync();
+
+while (true)
+{
+    Console.Write("> ");
+    var userInput = Console.ReadLine() ?? string.Empty;
+    AgentResponse response = await agent.RunAsync(userInput, session);
+    Console.WriteLine(response);
+    Console.WriteLine("=============================================");
+    Console.WriteLine($"Token usage: Input Token: {response.Usage!.InputTokenCount}, Output Token: {response.Usage!.OutputTokenCount}");
+}
+
+
